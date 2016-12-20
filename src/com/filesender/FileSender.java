cb7ded6937773b11ed0c9377204715da7ef55a84
Arkadiusz Ryszewski <a.ryszewski@outlook.com>
2016-12-20 16:20:18 +0100

    display empty frame

--- src/com/filesender/FileSender.java
+++ src/com/filesender/FileSender.java
@@ -1,10 +1,16 @@
 package com.filesender;
 
+import javax.swing.*;
+
 /**
  * Created by Arek on 20.12.2016.
  */
 public class FileSender {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        JFrame frame = new JFrame("File Sender");
+        frame.setSize( 800, 600); // Set frame size
+        frame.setLocationRelativeTo(null); // Put frame in center of the screen
+        frame.setVisible(true);
     }
 }
