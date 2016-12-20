b247f7bff90178972cf47a81193a7c67a923338d
Arkadiusz Ryszewski <a.ryszewski@outlook.com>
2016-12-20 23:45:57 +0100

    'obtain and display ip address in console'

--- src/com/filesender/FileSender.java
+++ src/com/filesender/FileSender.java
@@ -1,13 +1,21 @@
 package com.filesender;
 
 import javax.swing.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by Arek on 20.12.2016.
  */
 public class FileSender {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String MyIP = null;
+        try {
+            MyIP = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello! Your IP is : " + MyIP );
         JFrame frame = new JFrame("File Sender");
         frame.setSize( 800, 600); // Set frame size
         frame.setLocationRelativeTo(null); // Put frame in center of the screen
