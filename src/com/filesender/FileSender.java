c627c99b7ae966ca7d6829e6c09679d3493ea4a8
Arkadiusz Ryszewski <a.ryszewski@outlook.com>
2016-12-20 15:58:31 +0100

    Initial commit

---
+++ src/com/filesender/FileSender.java
@@ -0,0 +1,10 @@
+package com.filesender;
+
+/**
+ * Created by Arek on 20.12.2016.
+ */
+public class FileSender {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
