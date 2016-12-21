3f678634f2ac9530698d95b09ce085c33917a060
piotrmoren <p.moren15@gmail.com>
2016-12-21 03:20:03 +0100

    test of commit changes

--- src/com/filesender/FileSender.java
+++ src/com/filesender/FileSender.java
@@ -3,20 +3,39 @@ package com.filesender;
 import javax.swing.*;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
+import java.net.*;
+import java.io.*;
 /**
  * Created by Arek on 20.12.2016.
  */
 public class FileSender {
     public static void main(String[] args) {
-        String MyIP = null;
+        URL whatismyip = null;
         try {
-            MyIP = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e){ }
+            whatismyip = new URL("http://checkip.amazonaws.com");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String ip = null; //you get the IP as a String
+        try {
+            ip = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(ip);
+
         JFrame frame = new JFrame("File Sender");
         frame.setSize( 800, 600); // Set frame size
         frame.setLocationRelativeTo(null); // Put frame in center of the screen
-        JLabel IPlabel = new JLabel("Hello! Your IP is : " + MyIP);
+        JLabel IPlabel = new JLabel("Hello! Your IP is : " + ip);
         JPanel panel = new JPanel();
         panel.add(IPlabel);
         frame.add(panel);
