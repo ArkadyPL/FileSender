fd6754ba5c90e955ecce36839a7ae46da8082a45
ArkadyPL <a.ryszewski@outlook.com>
2017-01-18 00:45:55 +0100

    symmetric encryption key sent successfully - patch 1

--- src/com/filesender/HelperClasses/RSA.java
+++ src/com/filesender/HelperClasses/RSA.java
@@ -7,24 +7,8 @@ import java.io.*;
 import java.security.InvalidKeyException;
 
 public class RSA {
-    public static byte[] encrypt(Object obj){
-        byte[] encryptedObject = null;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutput out = null;
-        try {
-            out = new ObjectOutputStream(bos);
-            out.writeObject(obj);
-            out.flush();
-            encryptedObject = bos.toByteArray();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                bos.close();
-            } catch (IOException ex) {
-                // ignore close exception
-            }
-        }
+    public static byte[] encrypt(Object object){
+        byte[] plainText = globals.ToByte(object);
         byte[] cipherText = null;
         try {
             globals.cipher.init(Cipher.ENCRYPT_MODE, globals.remoteKey);
@@ -32,7 +16,7 @@ public class RSA {
             e.printStackTrace();
         }
         try {
-            cipherText = globals.cipher.doFinal(encryptedObject);
+            cipherText = globals.cipher.doFinal(plainText);
         } catch (IllegalBlockSizeException e) {
             e.printStackTrace();
         } catch (BadPaddingException e) {
@@ -55,24 +39,6 @@ public class RSA {
         } catch (BadPaddingException e) {
             e.printStackTrace();
         }
-        ByteArrayInputStream bis = new ByteArrayInputStream(plainText);
-        ObjectInput in = null;
-        Object decryptedObject = null;
-        try {
-            in = new ObjectInputStream(bis);
-            decryptedObject = in.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-            }
-        }
-        return decryptedObject;
+        return globals.ToObject(plainText);
     }
 }
--- src/com/filesender/HelperClasses/globals.java
+++ src/com/filesender/HelperClasses/globals.java
@@ -3,6 +3,7 @@ package com.filesender.HelperClasses;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.swing.*;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -29,4 +30,47 @@ public class globals {
     public static SecretKey symmetricKey = null;
     public static Cipher aesCipher = null;
     public static volatile boolean isConnected = false;
+
+    public static byte[] ToByte(Object object){
+        byte[] result = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = null;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(object);
+            out.flush();
+            result = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+        return result;
+    }
+
+    public static Object ToObject(byte[] enbytedObject){
+        ByteArrayInputStream bis = new ByteArrayInputStream(enbytedObject);
+        ObjectInput in = null;
+        Object result = null;
+        try {
+            in = new ObjectInputStream(bis);
+            result = in.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+            }
+        }
+        return result;
+    }
 }
