c11837b140ab83b24e9634297708c820942c0edc
ArkadyPL <a.ryszewski@outlook.com>
2017-01-17 20:33:35 +0100

    big change for try

--- src/com/filesender/ConnectionListener.java
+++ src/com/filesender/ConnectionListener.java
@@ -15,35 +15,30 @@ import java.security.interfaces.RSAPublicKey;
 
 
 public class ConnectionListener {
-    public static Socket ListenForIncomingConnections(TreeModel localTreeModel, ServerSocket serverSocket) throws IOException, ClassNotFoundException {
-        Socket connectedSocket;
+    public static void ListenForIncomingConnections(TreeModel localTreeModel) throws IOException, ClassNotFoundException {
         Log.Write("Waiting for incoming connections...");
 
-        connectedSocket = serverSocket.accept();
+        globals.connectionSocket = globals.serverSocket.accept();
 
         Log.Write("Connection accepted!");
 
-        ObjectInputStream inFromServer = new ObjectInputStream(connectedSocket.getInputStream());
+        ObjectInputStream inFromServer = new ObjectInputStream(globals.connectionSocket.getInputStream());
         Operation basicOp;
         basicOp = (Operation)inFromServer.readObject();
 
         if(basicOp.opID == 1) {//Rebuild tree for argument as a root
             Log.WriteTerminal("Operation ID 1 executed");
-            Sender.sendTree(connectedSocket,localTreeModel,serverSocket,basicOp);
+            Sender.sendTree(globals.connectionSocket,localTreeModel,basicOp);
         }
         else if(basicOp.opID == 2) {//Send given file
-            Sender.sendFile(basicOp.argument1,localTreeModel,connectedSocket,serverSocket);
+            Sender.sendFile(basicOp.argument1,localTreeModel,globals.connectionSocket);
         }else if(basicOp.opID == 5){//save new public key from arg1 and send your public key in arg1
             globals.remoteKey = (RSAPublicKey)basicOp.obj1;
             Log.WriteTerminal("Remote PublicKey:\n" + DatatypeConverter.printHexBinary(globals.remoteKey.getEncoded()));
 
-            ObjectOutputStream ostream = new ObjectOutputStream(connectedSocket.getOutputStream());
+            ObjectOutputStream ostream = new ObjectOutputStream(globals.connectionSocket.getOutputStream());
             Operation basicOperation = new Operation(6, null,null, globals.pubKey);
             ostream.writeObject(basicOperation);
-
-            ConnectionListener.ListenForIncomingConnections(localTreeModel, serverSocket);
         }
-
-        return connectedSocket;
     }
 }
