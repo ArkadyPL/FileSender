5d7d0b6cf715f70307b567df1b630987f897873d
ArkadyPL <a.ryszewski@outlook.com>
2017-01-18 10:55:45 +0100

    excluded action function

--- src/com/filesender/Connection.java
+++ src/com/filesender/Connection.java
@@ -1,15 +1,18 @@
 package com.filesender;
 
-import com.filesender.HelperClasses.Log;
-import com.filesender.HelperClasses.Operation;
+import com.filesender.HelperClasses.*;
 import com.filesender.Cryptography.RSA;
-import com.filesender.HelperClasses.globals;
 
 import javax.crypto.KeyGenerator;
+import javax.swing.*;
 import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPublicKey;
 
@@ -41,6 +44,51 @@ public class Connection {
         globals.symmetricKey = KeyGen.generateKey();
         Log.WriteTerminal("SymmetricKey:\n" + DatatypeConverter.printHexBinary(globals.symmetricKey.getEncoded()));
         ostream.writeObject(RSA.encrypt(globals.symmetricKey));
+    }
 
+    public static void connectToRemote(JTextField remoteIPTextField){
+        try {
+            globals.remoteIP = InetAddress.getByName(remoteIPTextField.getText());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        Boolean isValid = new IPAddressValidator().validate(remoteIPTextField.getText());
+        Log.WriteTerminal("Connection button clicked. Remote IP value: " + globals.remoteIP  + "\tGiven IP address is " + (isValid ? "valid" : "not valid"));
+        if(isValid) {
+            try {
+                Log.Write("Trying to connect to remote server...");
+                globals.connectionSocket = new Socket(globals.remoteIP, 9990);
+            } catch (ConnectException e) {
+                Log.Write("Remote is not available...");
+            } catch (Exception e) {
+                Log.Write("Connection error...");
+            }
+            if (globals.connectionSocket != null) {
+                globals.isConnected = true;
+                ServerStatus connectionStatusChecker = new ServerStatus();
+                connectionStatusChecker.setDaemon(true);
+                connectionStatusChecker.start();
+                Log.Write("Connected to remote!");
+                try {
+                    Connection.exchangeKeys();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    globals.connectionSocket = new Socket(globals.remoteIP, 9990);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Receiver.receiveTree(globals.remoteTree,globals.connectionSocket,"root",false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                globals.frame.repaint();
+                globals.frame.revalidate();
+            }
+        }
     }
 }
