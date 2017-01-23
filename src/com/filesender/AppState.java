2eafc4fa66398e40086b9c5d6848fd25de5713fb
ArkadyPL <a.ryszewski@outlook.com>
2017-01-23 23:48:52 +0100

    small improvement2

--- src/com/filesender/AppState.java
+++ src/com/filesender/AppState.java
@@ -1,6 +1,5 @@
 package com.filesender;
 
-import com.filesender.Cryptography.RSA;
 import com.filesender.HelperClasses.globals;
 
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -10,7 +9,6 @@ public class AppState {
     public static void changeToDisconnected(){
         globals.remoteIP = null;
         globals.previousDir = null;
-        RSA.remoteKey = null;
         globals.connectButton.setText("Connect");
         globals.remoteTree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("<No connection>")));
         globals.remoteIPTextField.setEditable(true);
